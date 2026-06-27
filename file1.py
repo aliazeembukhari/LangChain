1e362b0e8cf4a04f6f0f3526721d74bb991ebdbe
aliazeembukhari <aliazeembukhari93@gmail.com>
2026-06-27 16:30:20 +0500

    Update file1.py

--- file1.py
+++ file1.py
@@ -6,3 +6,8 @@ load_dotenv()
 llm = ChatGroq(model="llama-3.1-8b-instant")
 result = llm.invoke("Which is the National Animal of Pakistan")
 print(result.content)
+
+print()
+
+result2 = llm.invoke("Name the colors in Rainbow")
+print(result2.content)
