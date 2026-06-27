19ebd66bee66959570ad7a649a5975b7fe730470
aliazeembukhari <aliazeembukhari93@gmail.com>
2026-06-27 16:26:03 +0500

    Long Chain
    In this Model you will ask the Question and It will provide the suitable answer using other AI model

---
+++ file1.py
@@ -0,0 +1,8 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.1-8b-instant")
+result = llm.invoke("Which is the National Animal of Pakistan")
+print(result.content)
